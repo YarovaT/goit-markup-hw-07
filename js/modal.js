d1de60d09305798a6f6b92c86d8d64a259ed62f2
Tetiana Yarova <andronikova11@gmail.com>
2021-01-28 21:17:17 +0200

    BEm для Формы

--- js/modal.js
+++ js/modal.js
@@ -8,6 +8,6 @@ js:
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('backdrop--is-hidden');
   }
 })();
